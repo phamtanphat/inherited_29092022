40b60302dfd204b80c937a616fdc82e66f934404
phamtanphat <phatdroid94@gmail.com>
2022-10-25 18:16:30 +0700

    Initial commit

---
+++ lib/demo_build_context.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+class DemoBuildContextPage extends StatefulWidget {
+  const DemoBuildContextPage({Key? key}) : super(key: key);
+
+  @override
+  State<DemoBuildContextPage> createState() => _DemoBuildContextPageState();
+}
+
+class _DemoBuildContextPageState extends State<DemoBuildContextPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo Build Context"),
+      ),
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        child: Center(
+          child: Ongba(
+            child: Chame(
+              child: ConCai(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Ongba extends StatefulWidget {
+  Widget child;
+
+  Ongba({required this.child});
+
+  @override
+  State<Ongba> createState() => _OngbaState();
+}
+
+class _OngbaState extends State<Ongba> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("Ong Ba"),
+        widget.child
+      ],
+    );
+  }
+}
+
+class Chame extends StatefulWidget {
+  Widget child;
+
+  Chame({required this.child});
+
+  @override
+  State<Chame> createState() => _ChameState();
+}
+
+class _ChameState extends State<Chame> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("Cha me"),
+        widget.child
+      ],
+    );
+  }
+}
+
+class ConCai extends StatefulWidget {
+
+  @override
+  State<ConCai> createState() => _ConCaiState();
+}
+
+class _ConCaiState extends State<ConCai> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Con cái"),
+    );
+  }
+}
+
